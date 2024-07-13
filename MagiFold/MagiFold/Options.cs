984f712c42d9e07fc0e8690de777248eca34afa5
Victor <paradisusvic@gmail.com>
2024-07-13 18:10:42 -0400

    Add project files

---
+++ MagiFold/MagiFold/Options.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+
+namespace MagiFold
+{
+    public class Options
+    {
+        [Option('d', "directory", Default = ".", Required = true, HelpText = "The destination directory for the generated 'matches.txt', 'unmatched.txt' and 'collisions.txt' files.")]
+        public string directory { get; set; }
+    }
+}
---
+++ MagiFold/MagiFold/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+
+namespace MagiFold
+{
+    /// <summary>
+    /// Main class.
+    /// </summary>
+    class MainClass
+    {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            // TODO Add code
+        }
+    }
+}
